4422551e67b377d7bdbc12bad08615640aca618b
Melanie <melaniecurrie01@gmail.com>
2019-07-27 23:11:20 +0000

    array merge/sorting challenge completed

---
+++ merge.js
@@ -0,0 +1,9 @@
+function merge (num1, num2) {
+  let concat = num1.concat(num2);
+  let sort = concat.sort();
+  return sort;
+}
+
+console.log(merge([ 4, 5, 6 ], [ 1, 2, 3, 4 ]), "=?", [ 1, 2, 3, 4, 4, 5, 6 ]);
+console.log(merge([ 4 ], [ 2, 5, 8 ]), "=?", [ 2, 4, 5, 8 ]);
+console.log(merge([ 1, 2, 6 ], []), "=?", [ 1, 2, 6 ]);
